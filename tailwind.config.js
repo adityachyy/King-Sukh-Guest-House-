0ef3c24db602c1b58d50a159fcaac4a57c6428e3
Aditya kumar Choudhary <choudhaaditya765@gmail.com>
2024-08-26 14:54:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:
+{
+mullish:["Mulish" , "sans-serif"],
+Whitney:["Whitney"],
+Ginto:["Ginto"],
+ggSans:["ggSans"],
+
+},
+colors:
+{
+  deepBlue:"#02042a",
+  LightBlue:"#2b84e9",
+  LightBlue300:"#4b94ed",
+  LightBlue500:"#0b72e7",
+  greenLight:"#61cea6",
+  grayText: "#808597",
+  LightGray:"344a6c",
+  grayBlue:"#344a6c",
+  deepBlueHead:"#162f56",
+  gray2:"#525a76",
+
+}
+    },
+  },
+  plugins: [],
+}
+
